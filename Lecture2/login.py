5ec0babe2bfc1977d3eccd8db0c3b436797f43e5
erumkhalida <erumkhalida@users.noreply.github.com>
2019-12-30 22:58:21 +0500

    Create login.py

---
+++ Lecture2/login.py
@@ -0,0 +1,52 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import unittest
+import time
+from pages.loginpage import LoginPage
+from pages.registerpage import Registrer
+from pages.Dashboard import Dashboard
+
+class McK_login(unittest.TestCase):
+    
+    def setUp(self):
+        self.driver = webdriver.Chrome("C:/chromedriver.exe")
+        self.login = LoginPage(self.driver)
+        self.register = Registrer(self.driver)
+        self.dashboard = Dashboard(self.driver)
+
+    def test_login(self):
+        self.driver.get("https://courses.edx.org")
+        self.assertTrue(self.login.is_browser_on_the_hpage())
+        self.driver.find_element_by_link_text('Sign In').click()  
+
+        #Page login  
+        self.assertTrue(self.login.is_browser_on_the_loginpage())        
+        self.login.enter_username('erum.khalida@arbisoft.com')              
+        self.login.enter_password('thispassword_11')
+        self.login.submit_button()
+        self.assertTrue(self.dashboard.is_browser_on_the_page())
+        self.login.logout()
+
+
+        self.assertTrue(self.register.is_browser_on_the_hpage())
+        
+        #Page Register    
+        self.driver.find_element_by_link_text('Register').click()
+        self.assertTrue(self.register.is_browser_on_the_regpage())
+        self.register.regemail('')
+        self.register.regname('')
+        self.register.regusername('')
+        self.register.regpassword('')
+        self.register.regcountry('')
+        self.register.submitButton()
+        self.assertTrue(self.dashboard.is_browser_on_the_page())
+        self.register.logout()
+        self.assertTrue(self.register.is_browser_on_the_hpage())
+        
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
