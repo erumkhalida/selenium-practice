2f1e4d91d2dc0f78cc304ca10471f26ccedf2f85
erumkhalida <erumkhalida@users.noreply.github.com>
2019-12-31 14:28:00 +0500

    Update loginpage.py

--- Lecture2/pages/loginpage.py
+++ Lecture2/pages/loginpage.py
@@ -9,7 +9,7 @@ class LoginPage():
         self.username_id = "#login-email"
         self.password_id = "#login-password"
 
-    def is_browser_on_the_hpage(self):
+    def is_browser_on_the_Mpage(self):
         WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, '#footer-language-button')))
         return True
 
@@ -20,11 +20,9 @@ class LoginPage():
            
 
     def enter_username(self, username):
-        # self.driver.find_element_by_css_selector(self.username_id).clear()
         self.driver.find_element_by_css_selector(self.username_id).send_keys(username)
 
     def enter_password(self, password):
-        # self.driver.find_element_by_css_selector(self.password_id).clear() 
         self.driver.find_element_by_css_selector(self.password_id).send_keys(password)
 
     def submit_button(self):
